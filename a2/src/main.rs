de8d14cddc9e36106182b9509455fa66ed2570be
Alexa Hong <alexahong@Alexas-MacBook-Air.local>
2019-03-01 01:40:46 -0500

    got it to work

--- a2/src/main.rs
+++ a2/src/main.rs
@@ -6,14 +6,11 @@ TASKS
 3. Implement Exec loop ( switch dispatch)
 4. print last item in stack to stdout
 */
-//use std::env;
 use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::Read;
-use std::io::Write;
 use std::slice::Iter;
-use std::io::prelude::*;
 extern crate byteorder;
 use byteorder::{ByteOrder, ReadBytesExt, BigEndian};
 
@@ -81,47 +78,18 @@ pub enum Binop {
     Lt,  //Returns true if one i32 is less than another, otherwise false
     Eq,  //Returns true if one i32 is equal another, otherwise false
 }
-//need to reverse this to FromBin
-
-// pub trait FromBin
-// {
-//     fn from_bin(binary: &mut Iter<u8>)-> Self;
-// }
-
-
-// impl FromBin for u32
-// {
-//     fn from_bin(binary: &mut Iter<u8>)-> u32
-
-//     {
-        
-//         for j in binary.iter_mut(){
-            
-//         }
-//        //BigEndian::read_u32(&v)
-
-
-//     }
-    
-    
-// }
-// impl FromBin for Unary{
-//     fn from_bin(self) -> Unop{
-//         Instr::Unop::Neg
-//     }
-// }
 
 trait FromBin 
 {
-    fn from_bin(binary:&mut Iter<u8>) -> Self;
+    fn from_bin(mut binary:&mut Iter<u8>) -> Self;
 }
 
 impl FromBin for i32 
 {
-    fn from_bin(binary:&mut Iter<u8>) -> i32
+    fn from_bin(mut binary:&mut Iter<u8>) -> i32
     {
 
-        let mut buf: Vec<u8> = [0, 0, 0, 0].to_vec();
+        let mut buf: Vec<u8> = Vec::new();
         for x in 0..4 
         {
         buf.push(*binary.next().unwrap());
@@ -134,9 +102,9 @@ impl FromBin for i32
 
 impl FromBin for u32 
 {
-    fn from_bin(binary: &mut Iter<u8>) -> u32
+    fn from_bin(mut binary: &mut Iter<u8>) -> u32
     {
-        let mut buf: Vec<u8> = [0, 0, 0, 0].to_vec();
+        let mut buf: Vec<u8> = Vec::new();
         for x in 0..4 
         {
         buf.push(*binary.next().unwrap());
@@ -144,230 +112,143 @@ impl FromBin for u32
         }
         BigEndian::read_u32(&buf)
 
+
+
     }
 }
 
+#[test]
+fn test_u32_from_bin() {
+    let v1 = vec![0, 0, 0 ,5];
+    let mut b1 = v1.iter();
+    assert_eq!(<u32 as FromBin>::from_bin(b1.by_ref()), 5);
+    // More test cases like this
+}
+#[test]
+fn test_i32_from_bin() {
+    let v1 = vec![0, 0, 0 ,5];
+    let mut b1 = v1.iter();
+    assert_eq!(<i32 as FromBin>::from_bin(b1.by_ref()), 5);
+    // More test cases like this
+}
 
 impl FromBin for Val 
 {
     fn from_bin(mut binary:&mut Iter<u8>) -> Val
     {
-        let mut b = binary.next();
-        let vu = Some(vec![0b0000_0000]); // vunit
-        let vbt = Some(vec![0b0000_0010]); //vbooltrue
-        let vbf = Some(vec![0b0000_0011]); //vboolfalse
-        let vdf = Some(vec![0b0000_0101]); //vundef
-        let vi = Some(vec![0b0000_0001]); //vi32
-        let vl = Some(vec![0b0000_0100]); //vloc
-        let mut v1 : i32 = 0;
-        let mut v2 : u32 = 0;
 
-        match b
+        match binary.next()
         {
-            vu => Val::Vunit,
-            vi => Val::Vi32(v1),
-            vl => Val::Vloc(v2),
-            vbt => Val::Vbool(true),
-            vbf => Val::Vbool(false),
-            vdf => Val::Vundef,
-            _       => panic!("ERROR: Vundef"),
+            Some(0b0000_0000) => Val::Vunit,
+            Some(0b0000_0001) => Val::Vi32(<i32 as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_0100) => Val::Vloc(<u32 as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_0010) => Val::Vbool(true),
+            Some(0b0000_0011) => Val::Vbool(false),
+            Some(0b0000_0101) => Val::Vundef,
+            _       => panic!("ERROR: Val"),
         }
     }
 }
 
-fn test_val_from_bin() 
+// fn test_val_from_bin() 
+// {
+//     let v1 = vec![0];
+//     let mut buf = v1.iter();
+//     assert_eq!(Val::from_bin(&mut buf),Val::Vi32(20));
+//     // other test cases here
+// }
+
+impl FromBin for Unop
 {
-    let v1 = vec![0];
-    let mut buf = v1.iter();
-    assert_eq!(Val::from_bin(&mut buf),Val::Vi32(20));
-    // other test cases here
+    fn from_bin(mut binary: &mut Iter<u8>) -> Unop
+    {
+
+       match binary.next()
+       {
+         Some(0b0000_0000) => Unop::Neg,
+         _       => panic!("ERROR: Unop"),
+
+       }
+    }
 }
 
-// impl FromBin for Unop
-// {
-//     fn from_bin(binary: &mut Iter<u8>) -> Unop
-//     {
-//         vec![0b0000_0000]
-//     }
-// }
+impl FromBin for Binop
+{
+    fn from_bin(mut binary: &mut Iter<u8>) -> Binop
+    {
+     
+        match binary.next()
+        {
+            Some(0b0000_0000) => Binop::Add,
+            Some(0b0000_0010) => Binop::Sub,
+            Some(0b0000_0001) => Binop::Mul,
+            Some(0b0000_0011) => Binop::Div,
+            Some(0b0000_0100) =>  Binop::Lt,
+            Some(0b0000_0101) =>  Binop::Eq,
+            _   => panic!("ERROR: Binop"),
+        }
+    }
+}
 
-// impl FromBin for Binop
-// {
-//     fn from_bin(binary: &mut Iter<u8>) -> Binop
-//     {
-//         match binary 
-//         {
-//             Add => vec![0b0000_0000],
-//             Sub => vec![0b0000_0010],
-//             Mul => vec![0b0000_0001],
-//             Div => vec![0b0000_0011],
-//             Lt  => vec![0b0000_0100],
-//             Eq  => vec![0b0000_0101],
-//         }
-//     }
-// }
+impl FromBin for Instr
+{
+    fn from_bin(mut binary: &mut Iter<u8>) -> Instr
+    {
 
-// impl FromBin for Instr
-// {
-//     fn from_bin(binary: &mut Iter<u8>) -> Instr
-//     {
-//         match binary 
-//         {
-//             Push(i)     => { let mut v = vec![0b0000_0000];
-//                 v.append(&mut<Val as FromBin>::from_bin(i));
-//                 v},
-//             Pop         => vec![0b0000_0001],
-//             Peek(i)     => { let mut v = vec![0b0000_0000];
-//                 v.append(&mut<u32 as FromBin>::from_bin(i));
-//                 v},
-//             Unary(i)    => { let mut v = vec![0b0000_0000];
-//                 v.append(&mut<Unop as FromBin>::from_bin(i));
-//                 v},
-//             Binary(i)   => { let mut v = vec![0b0000_0000];
-//                 v.append(&mut<Binop as FromBin>::from_bin(i));
-//                 v},
-//             Swap        => vec![0b0000_0101],
-//             Alloc       => vec![0b0000_0110],
-//             Set         => vec![0b0000_0111],
-//             Get         => vec![0b0000_1000],
-//             Var(i)      => { let mut v = vec![0b0000_0000];
-//                 v.append(&mut<u32 as FromBin>::from_bin(i));
-//                 v},
-//             Store(i)    => { let mut v = vec![0b0000_0000];
-//                 v.append(&mut<u32 as FromBin>::from_bin(i));
-//                 v},
-//             SetFrame(i) => { let mut v = vec![0b0000_0000];
-//                 v.append(&mut<u32 as FromBin>::from_bin(i));
-//                 v},
-//             Call        => vec![0b0000_1100],
-//             Ret         => vec![0b0000_1101],
-//             Branch      => vec![0b0000_1110],
-//             Halt        => vec![0b0000_1111],
-//         }
-//     }
-// }
+      
+        match binary.next()
+        {
+
+            Some(0b0000_0000)  => Instr::Push(<Val as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_0001) => Instr::Pop,
+            Some(0b0000_0010) => Instr::Peek(<u32 as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_0011) => Instr::Unary(<Unop as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_0100) => Instr::Binary(<Binop as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_0101) => Instr::Swap,
+            Some(0b0000_0110) => Instr::Alloc,
+            Some(0b0000_0111) => Instr::Set,
+            Some(0b0000_1000) => Instr::Get,
+            Some(0b0000_1001) => Instr::Var(<u32 as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_1010) => Instr::Store(<u32 as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_1011) => Instr::SetFrame(<u32 as FromBin>::from_bin(&mut binary)),
+            Some(0b0000_1100) => Instr::Call,
+            Some(0b0000_1101) => Instr::Ret,
+            Some(0b0000_1110) => Instr::Branch,
+            Some(0b0000_1111) => Instr::Halt,
+             _   => panic!("ERROR: Instr"),
+  
+        }
+    }
+}
 
 
 fn main() {
 
     
-    //let args: Vec<String> = env::args().collect();
-
-	let mut string = String::new(); // string of what is in the .s file
-    let mut file = File::open("multi-arg.o").expect("file did not open");
-    //let buf = fs::read(&args[1])?; //if ? doesn't work use unwrap()
-   
-    let mut binvec: Vec<u8> = Vec::new(); // values from .o file 
-    file.read_to_end(&mut binvec).unwrap();
-println!("bin vec{:?}", binvec );
-    //file.read_to_string(&mut string).unwrap();
+    let args: Vec<_> = env::args().collect();
+    let mut file = String::new();
+    let file = args[1].to_string();
 
 
-
-   let mut buf = binvec.iter();
-   let prog_len = <Val as FromBin>::from_bin(&mut buf);
-   println!("bin vec{:?}", prog_len);
-
-   //  let mut sizeofvec = binvec[3] as usize;
-   // // println!("size: {:?}", sizeofvec );
-   //  let mut b = &binvec[0..4];//vector gets the first for bytes for size of 
-   //  //println!("{:?}", b);//b is a u8 and needs to be a usize
-   
-   //  let mut instrvec: Vec<Instr> = Vec::with_capacity(sizeofvec); // new vec to store instructions
-
-   //  let mut vec = instrvec;
-   //  //  for i in binvec{
-       
-   //  // }
-
-   //  //println!("vec_of_chars: {:?}", vec_of_chars);
-
-
-   //  let mut rest = &binvec[4..]; //gets the rest of vector
-   //  let mut five = &binvec[8..12]; //gets the rest of vector
-   //  //println!("{:?}", five);
-   //  let mut m = 0; 
-
-
-   // // let prog_len = <u32 as FromBin>::from_bin(five.to_vec());//how to call it
-
-   //  for i in 4..binvec.len() // i is the location in the binary vec, and binvec.len() is the total elements in binvec
-   //  {
-   //      println!("this is the first value {:?}",binvec[i]);
-
-   //      if m > 0
-   //      {
-   //          m = m - 1;
-   //      }
-   //      else{
-   //          //println!("{:?}", i);
-   //                          println!("this is the first value {:?}",binvec[i]);
-
-   //          match binvec[i]
-   //          {
-
-   //              11 =>//setframe
-   //              {
-   //                  println!("m value{:?}", m);
-   //                  // println!(" i value {:?}",i );
-   //                  // let set = &binvec[i+1..i+5];
-   //                  // println!("{:?}", set);
-   //                  // let B = BigEndian::read_u32(&set);
-   //                  // println!("{:?}", B);
-   //                  // vec.push(Instr::SetFrame(B));
-   //                  // println!("inside of vec{:?}", vec);
-   //                  //  m = m + 1;
-
-
-   //              },
-   //              15 => //halt
-   //              {
-                  
-   //                  vec.push(Instr::Halt);
-   //                  break;
-   //                  println!("inside of vec{:?}", vec);
-   //                                      m = m + 1;
-
-
-   //              },
-   //              // 0 => {// push
-   //              //     match binvec[i]{
-   //              //         0 => {//vunit
-   //              //             //i = i + 1;
-   //              //             m = m + 1;
-   //              //            vec.push(&Instr::Push(Val::Vunit));
-   //              //         },
-   //              //         1 => {//vi32
-   //              //            // i = i + 1;
-   //              //             m = m + 1;
-   //              //             vec.push(&Instr::Push(Val::Vi32(binvec[i] as i32)));
-   //              //         },
-   //              //         2 => {//vbooltrue
-
-   //              //         },
-   //              //         3 =>{//vboolfalse
-
-   //              //         },
-   //              //         4 => {//Vloc u32, Label
-                            
-   //              //         },
-   //              //         5 => {//Vundef
-
-   //              //         },
-   //              //         _ => panic!("oh no"),    
-   //              //     }
-
-   //              _ => panic!("oh no"),  
-   //              }
-
-   //          }//end of match1
-   //      }//end of else
-   //  }
-        
+    let mut binvec: Vec<u8> = fs::read(file).unwrap(); // values from .o file 
+    let mut instrvec: Vec<Instr> = Vec::new();// new vec to store instructions
+    let mut byte_iterator = binvec.iter();
+    let prog_len = <u32 as FromBin>::from_bin(byte_iterator.by_ref()); // first 4 bytes
     
+    for i in 0..prog_len
+    {
 
-    
-    //println!("this is what im looking for: {:?}\n",prog_len);
+        instrvec.push(Instr::from_bin(byte_iterator.by_ref()));
+
+
+    }
+
+
+    for p in instrvec
+    {
+         println!("{:?}",p );
+    }
+
 
 
 }
