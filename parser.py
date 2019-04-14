90e86ed82affd2958892a72ae66722af34419092
Alexa Hong <ah455013@ohio.edu>
2019-04-14 19:13:10 -0400

    updated

--- parser.py
+++ parser.py
@@ -5,64 +5,46 @@ import lexer
 
 tokens = lexer.tokens
 tok = lexer.tok
+def parser(lexer, current_token, rp_track, lp_track):
 
-def p_binary_operators(p):
-     '''expression : expression PLUS term
-                   | expression MINUS term
-        term       : term TIMES factor
-                   | term DIVIDE factor'''
-     if p[1] == '+':
-         p[0] = p[2] + p[3]
-     elif p[1] == '-':
-         p[0] = p[2] - p[3]
-     elif p[1] == '*':
-         p[0] = p[2] * p[3]
-     elif p[1] == '/':
-         p[0] = p[2] / p[3]
-
-# def p_expression_plus(p):
-#     'expression : expression PLUS term'
-#     p[0] = p[1] + p[3]
-
-# def p_expression_minus(p):
-#     'expression : expression MINUS term'
-#     p[0] = p[1] - p[3]
+  if tok.type == 'MOD':
+    rp_track = rp_track + 0
 
 
-def p_expression_term(p):
-    'expression : term'
-    p[0] = p[1]
+  elif tok.type == 'PLUS':
+    tok1 = lexer.token()
+    tok2 = lexer.token()
+    print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary +')
+    return lexer
 
-# def p_term_times(p):
-#     'term : term TIMES factor'
-#     p[0] = p[1] * p[3]
+      elif tok.type == 'MINUS':
+    tok1 = lexer.token()
+    tok2 = lexer.token()
+    print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary -')
+    return lexer
 
-# def p_term_div(p):
-#     'term : term DIVIDE factor'
-#     p[0] = p[1] / p[3]
+  elif tok.type == 'DIVIDE':
+    tok1 = lexer.token()
+    tok2 = lexer.token()
+    print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary /')
+    return lexer
 
-def p_term_factor(p):
-    'term : factor'
-    p[0] = p[2]
+  elif tok.type == 'TIMES':
+    tok1 = lexer.token()
+    tok2 = lexer.token()
+    print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary *')
+    return lexer
 
-def p_factor_num(p):
-    'factor : NUMBER'
-    p[0] = p[2]
 
-def p_factor_expr(p):
-    'factor : LPAREN expression RPAREN'
-    p[0] = p[3]
 
-# Error rule for syntax errors
-def p_error(p):
-    print ("Syntax error in input!")
 
 # Build the parser
-yacc.yacc()
+parser = yacc.yacc()
 
 
 # Use this if you want to build the parser using SLR instead of LALR
 # yacc.yacc(method="SLR")
+
 
 while True:
    try:
@@ -70,5 +52,5 @@ while True:
    except EOFError:
        break
    if not s: continue
-   result = yacc.parse(lexer = s)
+   result = parser.parse(s)
    print (result)
