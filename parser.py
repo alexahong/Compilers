46337c09671548abced1f3e3cdcd7216c7fa71e5
jm876913 <jm876913@ohio.edu>
2019-04-14 20:23:36 -0400

    fixed build error but still prints none

--- parser.py
+++ parser.py
@@ -7,36 +7,40 @@ tokens = lexer.tokens
 tok = lexer.tok
 def parser(lexer, current_token, rp_track, lp_track):
 
-  if tok.type == 'MOD':
-    rp_track = rp_track + 0
+    if tok.type == 'MOD':
+        rp_track = rp_track + 0
 
 
-  elif tok.type == 'PLUS':
-    tok1 = lexer.token()
-    tok2 = lexer.token()
-    print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary +')
-    return lexer
+    elif tok.type == 'PLUS':
+        tok1 = lexer.token()
+        tok2 = lexer.token()
+        print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary +')
+        return lexer
 
-      elif tok.type == 'MINUS':
-    tok1 = lexer.token()
-    tok2 = lexer.token()
-    print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary -')
-    return lexer
+    elif tok.type == 'MINUS':
+        tok1 = lexer.token()
+        tok2 = lexer.token()
+        print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary -')
+        return lexer
 
-  elif tok.type == 'DIVIDE':
-    tok1 = lexer.token()
-    tok2 = lexer.token()
-    print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary /')
-    return lexer
+    elif tok.type == 'DIVIDE':
+        tok1 = lexer.token()
+        tok2 = lexer.token()
+        print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary /')
+        return lexer
 
-  elif tok.type == 'TIMES':
-    tok1 = lexer.token()
-    tok2 = lexer.token()
-    print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary *')
-    return lexer
+    elif tok.type == 'TIMES':
+        tok1 = lexer.token()
+        tok2 = lexer.token()
+        print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary *')
+        return lexer
 
 
-
+def p_empty(p):
+    '''
+    empty :
+    '''
+    p[0] = None
 
 # Build the parser
 parser = yacc.yacc()
