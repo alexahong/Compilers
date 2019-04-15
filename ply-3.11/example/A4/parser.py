adfb59f4f7e94abe5bd48100747297117cff6292
Alexa Hong <ah455013@ohio.edu>
2019-04-15 18:50:37 -0400

    this is the working version:

--- ply-3.11/example/A4/parser.py
+++ ply-3.11/example/A4/parser.py
@@ -20,10 +20,12 @@ reserved = {
 'unit' : 'UNIT',
 'true' : 'TRUE',
 'false' : 'FALSE',
+'neg' : 'NEG',
 
 }
 
 tokens = (
+'NEG',
 'NUMBER',
 'PLUS',
 'MINUS',
@@ -31,8 +33,8 @@ tokens = (
 'DIVIDE',
 'LPAREN',
 'RPAREN',
-'RCOMMENT',
 'LCOMMENT',
+'RCOMMENT',
 'TRUE',
 'FALSE',
 'TT',
@@ -69,10 +71,9 @@ t_TIMES   = r'\*'
 t_DIVIDE  = r'\/'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
-#t_RCOMMENT = r'\*/'
-#t_LCOMMENT = r'\/*'
-
-# t_tt = r'tt'
+t_LCOMMENT = r'\/\*'
+t_RCOMMENT   = r'\*\/'
+# t_TT = r'\tt'
 t_EQUAL = r'\=='
 t_LT = r'\<'
 t_ASSIGN = r'\='
@@ -111,17 +112,19 @@ def t_error(t):
   t.lexer.skip(1)
  
 
-
+#%
 sep = False
+#given
 print("setframe 0")
 print("push Lmain")
 print("call")
 print("halt")
 print("Lmain:")
-def parser(lexer, current_token, rp_track, lp_track):
+
+def parser(lexer, current_token, lp_track, rp_track):
 
   if current_token.type == 'SEP':
-    rp_track = rp_track + 0
+    lp_track = lp_track + 0
     sep = True
 
   elif current_token.type == 'PLUS':
@@ -148,34 +151,101 @@ def parser(lexer, current_token, rp_track, lp_track):
     print("push "+ str(tok1.value) + "\npush " + str(tok2.value) + '\nbinary *')
     return lexer
 
+  elif current_token.type == 'LPAREN':
+    lp_track = lp_track + 1 
+    return lexer
+
+  elif current_token.type == 'RPAREN':
+    rp_track = rp_track + 1
+    return lexer
+
+  elif current_token.type == 'NUMBER':
+    print("push "+ str(current_token.value))
+    return lexer
+
+  elif current_token.type == 'NEG':
+    parser(lexer, lexer.token(), lp_track, rp_track)
+    print("unary neg")
+    return lexer
+
+  elif current_token.type == 'SEQ':
+    # tok1 = lexer.token()
+    # tok2 = lexer.token()
+    # print("push "+ str(tok2.value))
+    parser(lexer, lexer.token(), lp_track, rp_track)
+    parser(lexer, lexer.token(), lp_track, rp_track)
+    return lexer
+
+  elif current_token.type == 'TRUE':
+    print("push true")
+    return lexer
+
+  elif current_token.type == 'FALSE':
+    print("push false")
+    return lexer
+
+  elif current_token.type == 'LT':
+    tok1 = lexer.token()
+    tok2 = lexer.token()
+
+    if tok1.value < tok2.value:
+      print("push "+ str(tok1.value) + '\nbinary <')
+    else:
+      print("value is not smaller")
+    return lexer
+
+  elif current_token.type == 'EQUAL':
+    tok1 = lexer.token()
+    tok2 = lexer.token()
+
+    if tok1.value == tok2.value:
+      print( "push "+ str(tok1.value)+ '\nbinary ==' )
+    else:
+      print("doesnt ==")
+      return lexer
+
+  elif current_token.type == 'LCOMMENT':
+    comment = 0
+    while(True):
+      tok = lexer.token()
+      if tok.type == 'LCOMMENT':
+          comment = comment+ 1
+      elif tok.type == 'RCOMMENT' and comment == 1:
+          return lexer
+      elif tok.type == 'RCOMMENT' and comment != 1:
+          comment = comment - 1
+
+        
 
 
 
 
+#build Lexer
 lexer = lex.lex()
 
+#read file
 file = sys.argv[1]
 f = open(file, "r")
 file_contents = f.read()
-
-
-
 # Give the lexer some input
 lexer.input(file_contents)
 
 
-
-
-rp_track = 0
-lp_track = 0
-
-
-
 while True:
+  #reads in first token
   tok = lexer.token()
   if not tok: 
       break 
   #print(tok)
+  #tracking the left and right paranthesis, should be zero if they match
+  rp_track = 0
+  lp_track = 0
   parser(lexer, tok, rp_track, lp_track)
+  #checking to make sure that parenthesis match, shoud be zero
+  if lp_track != rp_track:
+    raise Exception("parenthesis don't match")
+  else:
+    print("ret")
 
-print("ret")
+
+
